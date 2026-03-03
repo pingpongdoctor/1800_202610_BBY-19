fe3dce00e65c4ed66131edde33fe2a983012187a
Carlos Fonseca <cadurcpf@gmail.com>
2026-03-02 18:41:12 -0800

    Fixed merge conflict with new navbar changes, refactored footer component to implement changes added

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -1,12 +1,12 @@
 class SiteFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <footer>
-                <a href="./index.html"><img class="footer-image" src="./images/Location.png" alt="location"></a>
-                <a href = "./challenges.html"> <img class="footer-image" src="./images/Footprint.png" alt="footprint"></a>
-                <a href = "./itemshop.html"> <img class="footer-image" src="./images//Dollar Bag.png" alt="shop"></a>
-                <a href = "/user.html"> <img class="footer-image" src="./images/Person.png" alt="person"></a>
-            </footer>
+            <nav>
+                <a href="/index.html"><img class="footer-image" src="/img/Location.png" alt="location"></a>
+                <a href = "/challenges.html"> <img class="footer-image" src="/img/Footprint.png" alt="footprint"></a>
+                <a href = "/itemshop.html"> <img class="footer-image" src="/img/Dollar Bag.png" alt="shop"></a>
+                <a href = "/user.html"> <img class="footer-image" src="/img/Person.png" alt="person"></a>
+            </nav>
         `;
     }
     
