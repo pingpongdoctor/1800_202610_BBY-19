28ae9efeaf405b81ac524a6cb42df0e5c2a56936
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-02 20:29:57 -0800

    delete the styles folder

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -2,7 +2,7 @@ class SiteFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav>
-                <a href="/index.html"><img class="footer-image" src="/img/Location.png" alt="location"></a>
+                <a href = "/index.html"><img class="footer-image" src="/img/Location.png" alt="location"></a>
                 <a href = "/challenges.html"> <img class="footer-image" src="/img/Footprint.png" alt="footprint"></a>
                 <a href = "/itemshop.html"> <img class="footer-image" src="/img/Dollar Bag.png" alt="shop"></a>
                 <a href = "/user.html"> <img class="footer-image" src="/img/Person.png" alt="person"></a>
