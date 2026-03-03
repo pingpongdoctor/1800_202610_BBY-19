7deacdbe5dd8151375daccd8e83f601bcd78a48c
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-02 21:30:11 -0800

    fix the relative paths

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -2,10 +2,10 @@ class SiteFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <nav>
-                <a href="/index.html"><img class="footer-image" src="/img/Location.png" alt="location"></a>
-                <a href = "/challenges.html"> <img class="footer-image" src="/img/Footprint.png" alt="footprint"></a>
-                <a href = "/itemshop.html"> <img class="footer-image" src="/img/Dollar Bag.png" alt="shop"></a>
-                <a href = "/user.html"> <img class="footer-image" src="/img/Person.png" alt="person"></a>
+                <a href="/index.html"><img class="footer-image" src="./public/images/Location.png" alt="location"></a>
+                <a href = "/challenges.html"> <img class="footer-image" src="./public/images/Footprint.png" alt="footprint"></a>
+                <a href = "/itemshop.html"> <img class="footer-image" src="./public/images/Dollar Bag.png" alt="shop"></a>
+                <a href = "/user.html"> <img class="footer-image" src="./public/images/Person.png" alt="person"></a>
             </nav>
         `;
     }
