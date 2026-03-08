2d7cb29394099e3249b7d0edcc1e495e40ab6a65
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-08 16:12:52 -0700

    incoporate Map API to implement the autocomplete searchbar feature

--- src/mapFunctions.js
+++ src/mapFunctions.js
@@ -1,16 +1,26 @@
 import { geocoding } from "@maptiler/client";
-
+import * as maptilersdk from '@maptiler/sdk';
 
 export async function getLocationsByPlaceName(name) {
     const result = await geocoding.forward(name);
-    return result;
+    return result?.features || [];
 }
 
-export async function getLocationsByPlaceNameAndCountry(name, country, limit) {
+export async function getLocationsByPlaceNameAndCountry(name, country, limit, proximity, bbox) {
+    console.log(name,country, limit, proximity);
     const result = await geocoding.forward(name, {
-        country:country,
-        limit:limit,
+        country,
+        limit,
+        proximity,
+        bbox,
+        types: ["poi"]
     });
-    return result;
+    return result?.features || [];
 }
 
+export async function addMarker(coordinates, map){
+    const marker = new maptilersdk.Marker()
+      .setLngLat(coordinates)
+      .addTo(map);
+      return marker
+}
