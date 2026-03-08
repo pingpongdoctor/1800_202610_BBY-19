00d4637d2c25ed549a199a408ed645392d3dd3c1
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-08 16:22:07 -0700

    add comments to map functions

--- src/mapFunctions.js
+++ src/mapFunctions.js
@@ -1,13 +1,15 @@
 import { geocoding } from "@maptiler/client";
 import * as maptilersdk from '@maptiler/sdk';
 
+// Function that is used to get Location information object using the place name
 export async function getLocationsByPlaceName(name) {
     const result = await geocoding.forward(name);
     return result?.features || [];
 }
 
+// Function that is used to get Location information object using the place name and the country
+// Proximity and bbox values allow setting the boundary of the search result
 export async function getLocationsByPlaceNameAndCountry(name, country, limit, proximity, bbox) {
-    console.log(name,country, limit, proximity);
     const result = await geocoding.forward(name, {
         country,
         limit,
@@ -18,6 +20,7 @@ export async function getLocationsByPlaceNameAndCountry(name, country, limit, pr
     return result?.features || [];
 }
 
+// Function that allows to set a new marker
 export async function addMarker(coordinates, map){
     const marker = new maptilersdk.Marker()
       .setLngLat(coordinates)
