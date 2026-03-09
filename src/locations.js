16fcda22378ba084d11d77f8506c7baa8a36cc21
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-08 20:13:36 -0700

    create an addNewLocation function

--- src/locations.js
+++ src/locations.js
@@ -1,16 +1,16 @@
 import { collection, getDocs, query, orderBy, addDoc, serverTimestamp } from 'firebase/firestore';
 import { db } from "./firebaseConfig.js";
+import { ln } from 'firebase/firestore/pipelines';
 
-//add locations
+//add sample locations
 function addSampleLocationData() {
-    console.log("running")
-    const hikesRef = collection(db, "locations");
-    addDoc(hikesRef, {
+    const locationsRef = collection(db, "locations");
+    addDoc(locationsRef, {
         name: "name1", description: "description1",
         type: "type1", lat: 49.2467097082573, lng: -122.9187029619698,
         last_updated: serverTimestamp()
     });
-    addDoc(hikesRef, {
+    addDoc(locationsRef, {
         name: "name2", description: "description2",
         type: "type2", lat: 49.2467097082573, lng: -122.9187029619698,
         last_updated: serverTimestamp()
@@ -22,4 +22,18 @@ function addSampleLocationData() {
 const locations = await getDocs(collection(db, "locations"))
 if (locations.empty) {
     addSampleLocationData();
+}
+
+// add new locaton
+function addNewLocation(name, description, type, lat, lng) {
+    const locationsRef = collection(db, "locations");
+    const location = {
+        name,
+        description,
+        type,
+        lat,
+        lng,
+        last_updated: serverTimestamp()
+    }
+    addDoc(locationsRef, location)
 }
