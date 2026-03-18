5c8740353aef4dfe8ba1da47e9c41bc08102206c
Alex Balog <slowcraik@gmail.com>
2026-03-18 14:54:14 -0700

    implement fully dynamic item shop database. connect purchased items to user, and update shop accordingly. change pathway for navbar depending on login state.

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -4,14 +4,16 @@ import { auth } from '../firebaseConfig.js'; // Firebase authentication connecti
 class SiteFooter extends HTMLElement {
     connectedCallback() {
         onAuthStateChanged(auth, (user) => {
-            const userPage = user ? 'profile.html' : 'login.html';
+            const userPage = user ? 'profile' : 'login';
+            const itemShopPage = user ? 'itemshop' : 'login';
+            const challengesPage = user ? 'challenges' : 'login';
 
             this.innerHTML = `
                 <nav class="navbar fixed-bottom border-top justify-content-around py-2">
                     <a href="/index.html"><img class="footer-image" src="/images/Location.png" alt="location"></a>
-                    <a href="/app/html/challenges.html"><img class="footer-image" src="/images/Footprint.png" alt="footprint"></a>
-                    <a href="/app/html/itemshop.html"><img class="footer-image" src="/images/Dollar Bag.png" alt="shop"></a>
-                    <a href="/app/html/${userPage}"><img class="footer-image" src="/images/Person.png" alt="person"></a>
+                    <a href="/app/html/${challengesPage}.html"><img class="footer-image" src="/images/Footprint.png" alt="footprint"></a>
+                    <a href="/app/html/${itemShopPage}.html"><img class="footer-image" src="/images/Dollar Bag.png" alt="shop"></a>
+                    <a href="/app/html/${userPage}.html"><img class="footer-image" src="/images/Person.png" alt="person"></a>
                 </nav>
             `;
         });
