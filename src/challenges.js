8f615811673c89ed9b868c8b0b5eab61cdb9e028
Alex Minty <mintya03@gmail.com>
2026-03-23 16:43:40 -0700

    started making the challenge list

--- src/challenges.js
+++ src/challenges.js
@@ -1,3 +1,4 @@
+console.log("Script loaded");
 import { db } from "./firebaseConfig.js";
 import { doc, onSnapshot } from "firebase/firestore";
 
@@ -21,8 +22,9 @@ function readChallengeGoal(challenge) {
     const challengeDocRef = doc(db, "challenges", challenge); // Get a reference to the document
 
     onSnapshot(challengeDocRef, docSnap => { // Listen for real-time updates
-        if (docSnap.exists()) {          //Document existence check
-            document.getElementById("challenge-goal").innerHTML = docSnap.data().challenge;
+        if (docSnap.exists()) {          // Document existence check
+            let challengeGoal = docSnap.data().goal;
+            return challengeGoal;
         } else {
             console.log("No such document!");
         }
@@ -31,15 +33,29 @@ function readChallengeGoal(challenge) {
     });
 }
 
-// Not yet functioning, meant to help make the challenges list. Rewrite if needed
-function displayChallenge (challenge) {
-    const list = document.getElementById("challengeList")
-    list.innerHTML = "";
+// Instantiates a challenge board item
+function createChallenge(title, value, max) {
+    let temp = document.querySelector("#challenge-item");
+    let cln = temp.content.cloneNode(true);
 
-    challenge.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = item.title;
+    let chalTitle = cln.querySelector("#challenge-title");
+    let progressBar = cln.querySelector(".progress-bar");
+    let progressContainer = cln.querySelector(".progress");
 
-        list.appendChild(li);
-    });
+    chalTitle.textContent = title;
+    progressBar.textContent = `${value} / ${max}`;
+    progressBar.style.width = `${(value / max) * 100}%`;
+    progressContainer.setAttribute("aria-valuenow", value);
+    progressContainer.setAttribute("aria-valuemax", max);
+
+    return cln;
 }
+const list = document.getElementById("challengeList");
+list.appendChild(createChallenge(readChallengeTitle("cafes"), 2, readChallengeGoal("cafes")));
+list.appendChild(createChallenge(readChallengeTitle("restaurants"), 2, readChallengeGoal("restaurants")));
+list.appendChild(createChallenge(readChallengeTitle("walking"), 2, readChallengeGoal("walking")));
+
+
+window.onload = function () {
+    readChallengeTitle("walking");
+};
