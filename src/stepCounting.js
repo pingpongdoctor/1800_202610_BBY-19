19129d527c1ea362126e57732a1e5effc1a64740
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-24 02:05:57 -0700

    feat: add the function that is used to update the points when users move

--- src/stepCounting.js
+++ src/stepCounting.js
@@ -1,4 +1,27 @@
 import * as maptilerClient from '@maptiler/client';
+import { getAuth } from "firebase/auth";
+import { updateDoc, getDoc } from 'firebase/firestore';
+
+const auth = getAuth();
+const user = auth.currentUser;
+
+if (user == null) {
+    if (!window.location.pathname.endsWith('index.html')) {
+        location.href = 'index.html';
+    }
+
+    return; // Stop execution
+}
+
+async function updateUserPoint(steps){
+    //get current points
+    const docRef = doc(db, "users", user.uid);
+    const userDoc = await getDoc(docRef);
+    const currentPoint = userDoc.data().points;
+    const updatedStep = currentPoint + steps;
+    //update points using the current points and new steps
+    await updateDoc(docRef, {points: updatedStep})
+}
 
 // This function is used to guess what kinds of transportation the users are using based on their current speed
 // The unit is m/s
@@ -35,8 +58,8 @@ navigator.geolocation.watchPosition((pos) => {
         return;
     }
     lastCalculation = now;
-    
-    if(lastPos == null){
+
+    if (lastPos == null) {
         console.log("user has not had previous coordinates to calculate the distance")
     }
 
@@ -53,15 +76,16 @@ navigator.geolocation.watchPosition((pos) => {
         const mode = detectTransportMode(speed);
 
         // If users are walking, we can start counting steps
-        if(mode == "walking"){
-            distanceToSteps(distance);
+        if (mode == "walking") {
+            const stepNum = distanceToSteps(distance);
             console.log(distanceToSteps(distance))
+            updateUserPoint(stepNum);
         }
     }
 
     // Set new values for last position and last time of the previous coordinates
     lastPos = pos.coords;
     lastTime = now;
-}, (error)=>{
+}, (error) => {
     console.log("Geolocation API watchPostion method error" + error)
-}, {enableHighAccuracy: true});
+}, { enableHighAccuracy: true });
