65743d8138e5524ceb110ba2968dc8b70fba3ab6
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-03-31 01:28:58 -0700

    feat: update challenge field in user document if user move close to a place

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,9 @@
-console.log("Script loaded");
 import { db } from "./firebaseConfig.js";
-import { doc, getDoc, collection, getDocs, query, where, updateDoc, arrayUnion, increment } from "firebase/firestore";
+import { doc, getDoc, collection, getDocs, query, updateDoc } from "firebase/firestore";
 import { onAuthReady } from '/src/authentication.js';
-export { queryMap } from "./components/site-searchbar.js"
+import { geocoding, config } from "@maptiler/client";
 
-
+config.apiKey = import.meta.env.VITE_MAPTILER_KEY;
 
 const challengeItem = document.getElementById("challengeItem");
 const itemList = document.getElementById("challengeList");
@@ -55,16 +54,17 @@ async function populateItems() {
     })
 }
 
-// Function that is used to recognize if users move close to a certain type of location
-// Types must be either cafe or restaurant for now
-async function moveCloseTo(type) {
-    if (Object.keys(queryMap).indexOf(type) == -1) {
-        console.log("Types are not supported")
-    }
+const restaurantTypes = ["restaurant", "food", "dining", "eatery", "diner", "bistro", "grill", "kitchen"];
+const cafeTypes = ["coffee", "cafe", "espresso", "coffeehouse", "coffee shop", "tea house", "bakery", "roastery"];
+
+// Function that is used to recognize if users move close to a cafe or a restaurant
+// Update the user database if the users move close these places
+async function moveCloseToCafe(userRef, userCafeChallenges, cafeGoal) {
     const userLng = window._userPosition?.lng;
     const userLat = window._userPosition?.lat;
 
     if (!userLng || !userLat) {
+        console.log("can not track user");
         return;
     };
 
@@ -80,12 +80,12 @@ async function moveCloseTo(type) {
     ];
 
     // Get the queries data for the location type input
-    const typeList = queryMap[type];
 
-    let moveClose = false;
+    let moveCloseCafe = false;
 
-    for (const type of typeList) {
-        const result = await geocoding.forward("cafe", {
+
+    for (const cafeType of cafeTypes) {
+        const result = await geocoding.forward(cafeType, {
             proximity: [userLng, userLat], // Set current user location to proximity to look for locations around the user
             bbox,
             types: ["poi"],
@@ -95,34 +95,125 @@ async function moveCloseTo(type) {
         const locations = result?.features || [];
 
         if (locations.length > 0) {
-            console.log(`user just move close to a ${type}`)
-            moveClose = true;
+            console.log(`user just move close to a ${cafeType}`)
+            moveCloseCafe = true;
             break;
         }
     }
 
-    return moveClose;
-
-
-
-}
-
-// Function to check types of challenges that the current user to start tracking user location as well as their progresses
-async function trackUserChallengeProgresses() {
-    const auth = getAuth();
-    const user = auth.currentUser;
-
-    if (user == null) {
-        console.log("User is not authenticated");
-        return;
+    if(!moveCloseCafe){
+        console.log("user do not move to any close cafe")
     }
 
-    //get current points
-    const docRef = doc(db, "users", user.uid);
-    const userDoc = await getDoc(docRef);
-    console.log(userDoc)
+
+    if (moveCloseCafe && (userCafeChallenges < cafeGoal)) {
+        // Increment user cafe challenge field if user moves close to a cafe and the goal has not been achieved
+        await updateDoc(userRef, {
+            challengeCafes: userCafeChallenges + 1
+        });
+        console.log("user userCafeChallenges is updated")
+    }
 }
 
-trackUserChallengeProgresses()
+async function moveCloseToRestaurant(userRef, userRestaurantChallenges, restaurantGoal) {
+    const userLng = window._userPosition?.lng;
+    const userLat = window._userPosition?.lat;
+
+    if (!userLng || !userLat) {
+        console.log("can not track user");
+        return;
+    };
+
+    // Convert 50m radius to degrees
+    const offset = 0.00045;
+
+    // Calculate the bounding box to search for a location type in 50m radius
+    const bbox = [
+        userLng - offset,
+        userLat - offset,
+        userLng + offset,
+        userLat + offset
+    ];
+
+    // Get the queries data for the location type input
+
+    let moveCloseRestaurant = false;
+
+    for (const restaurantType of restaurantTypes) {
+        const result = await geocoding.forward(restaurantType, {
+            proximity: [userLng, userLat],
+            bbox,
+            types: ["poi"],
+            limit: 10
+        });
+
+        const locations = result?.features || [];
+
+        if (locations.length > 0) {
+            console.log(`user just move close to a ${restaurantType}`)
+            moveCloseRestaurant = true;
+            break;
+        }
+    }
+
+    if(!moveCloseRestaurant){
+        console.log("user does not move close to any restaurant")
+    }
+
+    if (moveCloseRestaurant && (userRestaurantChallenges < restaurantGoal)) {
+        // Increment user cafe challenge field if user moves close to a cafe and the goal has not been achieved
+        await updateDoc(userRef, {
+            challengeRestaurants: userRestaurantChallenges + 1
+        });
+        console.log("user challengeRestaurants is updated")
+    }
+}
+
+
+let trackUserChallengesInterval;
+
+async function trackUserMoveCloseToChallengePlaces(user) {
+   
+        // If no user is logged in, redirect to the login page
+        if (!user) {
+            if (!window.location.pathname.endsWith('index.html')) {
+                location.href = 'index.html';
+            }
+            return; // Stop execution
+        }
+
+        // Get the challenges points that users have obtained
+        const userRef = doc(db, "users", user.uid);
+        const userDoc = await getDoc(userRef, user.uid);
+        const userData = userDoc.data();
+
+        const userCafeChallenges = userData.challengeCafes;
+        const userRestaurantChallenges = userData.challengeRestaurants;
+
+        // Get the goal of each challenges from the challenges collection
+        const cafeChallengesDocSnap = await getDoc(doc(db, "challenges", "challengeCafes"));
+        const cafeGoal = cafeChallengesDocSnap.data().goal;
+        const restaurantsChallengesDocSnap = await getDoc(doc(db, "challenges", "challengeRestaurants"));
+        const restaurantGoal = restaurantsChallengesDocSnap.data().goal;
+        
+        if (userCafeChallenges < cafeGoal) {
+            moveCloseToCafe(userRef, userCafeChallenges, cafeGoal)
+        }
+
+        if (userRestaurantChallenges < restaurantGoal) {
+            moveCloseToRestaurant(userRef, userRestaurantChallenges, restaurantGoal)
+        }
+
+        if((userCafeChallenges >= cafeGoal) && (userRestaurantChallenges >= restaurantGoal)){
+            if(trackUserChallengesInterval){
+                clearInterval(trackUserChallengesInterval);
+                trackUserChallengesInterval = null;
+            }
+        }
+}
+
+ onAuthReady(async (user) => {
+    trackUserChallengesInterval = setInterval(()=>{trackUserMoveCloseToChallengePlaces(user)}, 5000);
+ })
 
 populateItems();
